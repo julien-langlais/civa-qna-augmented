6983f7b4b1ac069e298f98711a7db748d0343af6
TCHALA <tchala@tchala-Aspire-E5-575G>
2021-03-25 01:16:24 +0100

    Updated the initialization of the bots (standard and augmented)

--- data/faq/update-nlu-and-domain.py
+++ data/faq/update-nlu-and-domain.py
@@ -2,10 +2,9 @@
 #-*- coding: UTF-8 -*-
 
 import pandas as pd
-import unicodedata
-path = r"data/data-base-source/QNA_for_bots.csv
-data_base = pd.read_csv(path,  sep=";", encoding="latin")
-with open("data/faq/faq-nlu.yml", "wt", encoding="utf-8") as f:
+path = r"data/data-base-source/QNA_for_bots.csv"
+data_base = pd.read_csv(path,  sep=";", encoding="latin3")
+with open("data/faq/faq-nlu.yml", "wt", encoding="latin3") as f:
     f.write('version: "2.0" \n')
     f.write('\n')
     f.write("nlu:\n")
@@ -15,6 +14,20 @@ with open("data/faq/faq-nlu.yml", "wt", encoding="utf-8") as f:
         f.write("  examples: |\n")
         for row in range(len(data_base_pivot)):
             text = str(data_base_pivot.iloc[row,1])
-            text = unicodedata.normalize('NFKD', text).encode('ASCII', 'ignore').decode("utf-8")
             f.write(f"   - {text}\n")
+        f.write("\n")
+
+with open("data/faq/faq-domain.yml", "wt", encoding="latin3") as f:
+    f.write('version: "2.0" \n')
+    f.write('\n')
+    f.write("intents:\n") 
+    f.write("  - faq\n")
+    f.write('\n')
+    f.write('responses:\n')
+    for num_pivot in range(1,len(data_base)):
+        data_base_pivot = data_base.loc[data_base["id_question_pivot"]==num_pivot,]
+        f.write("  utter_faq/" + str(num_pivot) +":" "\n")
+        for row in range(len(data_base_pivot)):
+            text = '"'+ str(data_base_pivot.iloc[row,2]) + '"'
+            f.write(f"    - text: {text}\n")
         f.write("\n")
